3c205fbf4e3aad79ffff453bf183c6ce1980a06c
mahmoodsheikh36 <mahmod.m2015@gmail.com>
2025-03-11 11:52:46 +0200

    use nixos-unstable for nixpkgs

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "nix-on-droid system config.";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     home-manager = {
       url = "github:nix-community/home-manager/release-24.05";
       inputs.nixpkgs.follows = "nixpkgs";
